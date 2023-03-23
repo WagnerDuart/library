591ff16e0105148d3d08394fc641fe8d89483aaa
WagnerDuart <124386335+WagnerDuart@users.noreply.github.com>
2023-03-23 13:17:39 -0300

    .

---
+++ books/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from uuid import uuid4
+
+
+# Create your models here.
+
+class Books(models.Model):
+    id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)  # id do livro
+    title = models.CharField(max_length=255)  # titulo do livro
+    author = models.CharField(max_length=255)  # autor
+    release_year = models.IntegerField()  # ano de lamçamento
+    state = models.CharField(max_length=50)  # estado do livro
+    pages = models.IntegerField()  # quantidade de paginas
+    create_at = models.DateField(auto_now_add=True)  # o dia de criação na base de dados
+    publishing_conpany = models.CharField(max_length=255)  # editora
